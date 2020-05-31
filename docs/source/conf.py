41783dbca001ab9b4191418ad44589fca0fcdec4
salvatore <militesalvatore@gmail.com>
2020-05-31 22:48:23 +0200

    Minimum working product

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ release = '0.1'
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
