8198742857f9aaaffd5d622dea6097bc9ee1275f
salvatore <militesalvatore@gmail.com>
2020-06-06 11:59:00 +0200

    just moved files

--- anneal/test/test_loss_down.py
+++ anneal/test/test_loss_down.py
@@ -5,15 +5,19 @@ from anneal.models.MixtureGaussian import MixtureGaussian
 from anneal.models.MixtureGaussianDMP import MixtureGaussianDMP
 from anneal.models.MixtureDirichlet import MixtureDirichlet
 from anneal.models.HmmMIxtureRNA import HmmMixtureRNA
+import os
 
 LR = 0.05
 ST = 2
 ST2 = 2
-
+data_dir = "anneal/data"
 
 class LossTest(unittest.TestCase):
     def test_GaussianMixture(self):
-        data_dict = anneal.utils.load_simulation_seg("./data", "example1")
+        print(os.curdir)
+
+        data_dict = anneal.utils.load_simulation_seg(data_dir, "example1")
+
 
         params, loss = anneal.utils.run_analysis(data_dict, MixtureGaussian, steps=ST, lr=LR)
         self.assertNotEqual(loss[1], loss[0])
@@ -29,7 +33,7 @@ class LossTest(unittest.TestCase):
 
 
     def test_GaussianMixtureDPM(self):
-        data_dict = anneal.utils.load_simulation_seg("./data", "example1")
+        data_dict = anneal.utils.load_simulation_seg(data_dir, "example1")
 
         params, loss = anneal.utils.run_analysis(data_dict, MixtureGaussianDMP, steps=ST, lr=LR)
         self.assertNotEqual(loss[1], loss[0])
@@ -46,7 +50,7 @@ class LossTest(unittest.TestCase):
         self.assertNotEqual(loss[1], loss[0])
 
     def test_DirichletMixture(self):
-        data_dict = anneal.utils.load_simulation_seg("./data", "example1")
+        data_dict = anneal.utils.load_simulation_seg(data_dir, "example1")
 
         params, loss = anneal.utils.run_analysis(data_dict, MixtureDirichlet, steps=ST, lr=LR)
         self.assertNotEqual(loss[1], loss[0])
@@ -65,7 +69,7 @@ class LossTest(unittest.TestCase):
         self.assertNotEqual(loss[1], loss[0])
 
     def test_HmmSimple(self):
-        data_dict = anneal.utils.load_simulation_seg("./data", "example1")
+        data_dict = anneal.utils.load_simulation_seg(data_dir, "example1")
 
         params, loss = anneal.utils.run_analysis(data_dict, HmmSimple, steps=ST, lr=LR)
         self.assertNotEqual(loss[1], loss[0])
@@ -73,7 +77,7 @@ class LossTest(unittest.TestCase):
 
 
     def test_HmmMixtureRNA(self):
-        data_dict = anneal.utils.load_simulation_seg("./data", "example1")
+        data_dict = anneal.utils.load_simulation_seg(data_dir, "example1")
 
         params, loss = anneal.utils.run_analysis(data_dict, HmmMixtureRNA, steps=ST, lr=LR)
         self.assertNotEqual(loss[1], loss[0])
