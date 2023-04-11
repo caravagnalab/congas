af0e2fbdbb25d2af62f0c18394710c9d15782013
Salvatore Milite <militesalvatore@gmail.com>
2023-04-11 19:59:36 +0200

    new release

--- congas/models/__init__.py
+++ congas/models/__init__.py
@@ -1,15 +1,11 @@
 from congas.models.HmmSimple import HmmSimple
-from congas.models.MixtureDirichlet import MixtureDirichlet
-from congas.models.MixtureGaussianDMP import MixtureGaussianDMP
 from congas.models.MixtureGaussian import MixtureGaussian
-from congas.models.HmmMixtureRNA import HmmMixtureRNA
-from congas.models.MixtureGaussianNorm import MixtureGaussianNorm
 from congas.models.HmmSegmenter import HmmSegmenter
-from congas.models.MixtureGaussianEXP import MixtureGaussianEXP
-from congas.models.MixtureGaussianOld import MixtureGaussianOld
+from congas.models.MixtureGaussianNorm import MixtureGaussianNorm
 
 
 
 
-__all__ = ["HmmSimple", "MixtureGaussian", "MixtureGaussianDMP", "MixtureDirichlet",
-           "HmmMixtureRNA", "MixtureGaussianNorm", "HmmSegmenter", "MixtureGaussianEXP", "MixtureGaussianOld"]
+
+__all__ = ["HmmSimple", "MixtureGaussian",
+           "HmmSegmenter", "MixtureGaussianNorm"]
